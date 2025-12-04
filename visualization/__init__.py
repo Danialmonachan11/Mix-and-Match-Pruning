9812a32ba90aef75d91fcaf9202eb848f934742c
Danial Monachan <51749222+Danialmonachan11@users.noreply.github.com>
2025-12-04 13:39:41 +0100

    Delete visualization directory

--- visualization/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Visualization modules for GA and benchmarking results."""
-
-from .ga_plots import GAPlotter
-from .benchmark_plots import BenchmarkPlotter
-
-__all__ = ['GAPlotter', 'BenchmarkPlotter']
